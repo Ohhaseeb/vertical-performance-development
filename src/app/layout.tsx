b25e52c811869b57471cb883ec6213690028720b
Ohhaseeb <haseeb.sayed@sjsu.edu>
2025-04-21 13:51:48 -0700

    moved analytics component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,8 +30,8 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <Toaster position="top-center" />
         <Analytics />
+        <Toaster position="top-center" />
       </body>
     </html>
   );
